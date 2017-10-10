ec004e2f9324b1c41fc57e4ad6a16d1cd7ccc8ab
Erik J. Johnson <erikjson@elemental.com>
2017-10-10 11:18:54 -0700

    Version 3.0 fixes
    - Compilation in release mode fixes
    - Bumped build and version numbers

--- thirdpartysource/PasswordSafe/os/typedefs.h
+++ thirdpartysource/PasswordSafe/os/typedefs.h
@@ -114,7 +114,14 @@ typedef wchar_t TCHAR;
 // mimic Microsoft conventional typdefs:
 typedef TCHAR *LPTSTR;
 typedef const TCHAR *LPCTSTR;
+#if OBJC_BOOL_IS_BOOL
 typedef bool BOOL;
+#else
+#   define OBJC_BOOL_IS_CHAR 1
+typedef signed char BOOL;
+// BOOL is explicitly signed so @encode(BOOL) == "c" rather than "C"
+// even if -funsigned-char is used.
+#endif
 typedef unsigned char BYTE;
 typedef uint16_t WORD;
 typedef uint32_t DWORD;
